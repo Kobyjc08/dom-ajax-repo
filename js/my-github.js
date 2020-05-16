6f338e15f3378cdfdfed54612bdd9fd14376a2c4
Jorge Cobo <kobyjc@hotmail.com>
2020-05-16 17:53:25 +0200

    part1done

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,20 @@
 // Write code here to communicate with Github
+let reposList = document.getElementById("repos-list");
+let countRepos = document.getElementById("repos-count");
+fetch('https://api.github.com/users/Kobyjc08/repos')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const numberOfRepos = data.length;
+        countRepos.innerText = numberOfRepos;
+
+        data.forEach(element => {
+            let itemList = document.createElement("li");
+            let itemListLink = document.createElement("a");
+            itemListLink.setAttribute("href", element.html_url);
+            itemListLink.innerText = element.name;
+            reposList.appendChild(itemList);
+            itemList.appendChild(itemListLink);
+
+        })
+    });
